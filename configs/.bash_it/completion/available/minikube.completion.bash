733764558b564c55d00d00be30a88c50246774db
prorok <artkor@live.ru>
2019-12-22 16:52:00 +0200

    configurations for my new Clear Linux installation

--- configs/.bash_it/completion/available/minikube.completion.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# minikube (Local Kubernetes) completion
-
-if command -v minikube &>/dev/null
-then
-  eval "$(minikube completion bash)"
-fi
--- configs/.bash_it/plugins/available/rbenv.plugin.bash
+++
@@ -1,7 +0,0 @@
-cite about-plugin
-about-plugin 'load rbenv, if you are using it'
-
-export RBENV_ROOT="$HOME/.rbenv"
-pathmunge "$RBENV_ROOT/bin"
-
-[[ `which rbenv` ]] && eval "$(rbenv init - bash)"
